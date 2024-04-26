908550704a01f30d7b50df1f5e4da1b3f1e633e1
2heeng <yiheeng0104@gmail.com>
2024-04-26 17:17:50 +0900

    240425 글제목에 댓글수 띄우기

--- src/main/java/org/zerock/b01/controller/ReplyController.java
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,19 +55,37 @@ public class ReplyController {
     }
 
 
+//    @ApiOperation(value = "Replies POST", notes = "Post 방식으로 댓글 등록")
+//    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE) //해당 메소드를 받아서 소비(consume)하는 데이터가 어떤 종류인지 명시, JSON 데이터 타입을 처리하는 메소드
+//    public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO){
+//        //@RequestBody는 JSON 문자열을 ReplyDTO로 변환
+//
+//        Map<String,Long> resultMap = new HashMap<>();
+//        resultMap=Map.of("rno",111L);
+//        //resultMap.put("replyDTO",replyDTO.getBno());
+//
+//        log.info("replyDTO: "+replyDTO);
+//
+//        return ResponseEntity.ok(resultMap);
+//
+//    }
+
+
+
     @ApiOperation(value = "Replies POST", notes = "Post 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE) //해당 메소드를 받아서 소비(consume)하는 데이터가 어떤 종류인지 명시, JSON 데이터 타입을 처리하는 메소드
-    public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO){
-        //@RequestBody는 JSON 문자열을 ReplyDTO로 변환
+    public Map<String,Long> register(@RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws Exception{
+
+
+        if(bindingResult.hasErrors()){
+            throw new BindException(bindingResult);
+        }
 
         Map<String,Long> resultMap = new HashMap<>();
-        resultMap=Map.of("rno",111L);
-        //resultMap.put("replyDTO",replyDTO.getBno());
 
-        log.info("replyDTO: "+replyDTO);
+        resultMap.put("rno",111L);
 
-        return ResponseEntity.ok(resultMap);
-
+        return resultMap;
     }
 
 }
--- src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
+++ src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.repository.search;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.b01.domain.Board;
 import org.zerock.b01.domain.QBoard;
+import org.zerock.b01.domain.QReply;
+import org.zerock.b01.dto.BoardListReplyCountDTO;
 
 import java.util.List;
 
@@ -79,5 +82,66 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
 
         return new PageImpl<>(list, pageable,count);
         //return null;
+    }
+
+    @Override
+    public Page<BoardListReplyCountDTO> searchWithReplyCount(String[] types, String keyword, Pageable pageable) {
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+
+        JPQLQuery<Board> query=from(board);
+
+
+        //게시물과 댓글의 경우 한쪽에만 데이터가 존재하는 상황이 발생
+        //그래서 outer join을통해서 처리함
+        query.leftJoin(reply).on(reply.board.eq(board));
+
+        //조인 후에 게시물당 처리가 필요하므로 groupBy를 적용
+        query.groupBy(board);
+
+
+        //검색조건
+        if( (types != null && types.length > 0) && keyword != null ){
+
+            BooleanBuilder booleanBuilder = new BooleanBuilder(); // (
+
+            for(String type: types){
+
+                switch (type){
+                    case "t":
+                        booleanBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        booleanBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        booleanBuilder.or(board.writer.contains(keyword));
+                        break;
+                }
+            }//end for
+            query.where(booleanBuilder);
+        }
+
+        //bno > 0
+        query.where(board.bno.gt(0L));
+
+        //Projections.bean() :
+        JPQLQuery<BoardListReplyCountDTO> dtoQuery=query.select(Projections.bean(
+                BoardListReplyCountDTO.class,
+                board.bno,
+                board.title,
+                board.writer,
+                board.regDate,
+                reply.count().as("replyCount")
+        ));
+
+        this.getQuerydsl().applyPagination(pageable,dtoQuery);
+
+        List<BoardListReplyCountDTO> dtoList = dtoQuery.fetch();
+
+        long count = dtoQuery.fetchCount();
+
+        return new PageImpl<>(dtoList, pageable, count);
     }
 }
